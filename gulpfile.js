ca74926dc55289080b2eff4fb22cfeebfc829e14
Oleksii Sapon <asapon@freeconferencecall.com>
2017-12-16 02:03:18 +0200

    Got manifest props from package.json

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,14 @@ const gulp = require('gulp');
 const license = require('gulp-header-license');
 const zip = require('gulp-zip');
 const fs = require('fs');
+const replace = require('gulp-token-replace');
 const packageJson = require('./package.json');
 
 gulp.task('license', function () {
     const headerProperties = {
         year: new Date().getFullYear(),
         author: packageJson.author,
-        webSite: packageJson.repository.url.slice(0, -4),
+        webSite: packageJson.homepage,
         license: packageJson.license
     };
 
@@ -17,8 +18,21 @@ gulp.task('license', function () {
         .pipe(gulp.dest('src'));
 });
 
-gulp.task('zip', function () {
-    gulp.src('src/**')
+gulp.task('injectData', function () {
+    const properties = {
+        extension: {
+            description: packageJson.description,
+            version: packageJson.version,
+            author: packageJson.author,
+        }
+    };
+    return gulp.src(['src/**'])
+        .pipe(replace({tokens : properties}))
+        .pipe(gulp.dest('build/injectData'))
+});
+
+gulp.task('zip', ['injectData'], function () {
+    gulp.src('build/injectData/**')
         .pipe(zip('stackoverflow-visitor-' + packageJson.version + '.zip'))
         .pipe(gulp.dest('build/dist'))
 });
